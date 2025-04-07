c9a24e8b996e8251f7a80747ed7f5da844cb66d9
Logan-01-for <sjson0105@gmail.com>
2025-04-07 17:50:13 +0900

    0 4 0 7

---
+++ JSP2/src/main/java/edu/kh/jsp2/dto/Book.java
@@ -0,0 +1,21 @@
+package edu.kh.jsp2.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//@Getter
+//@Setter
+//@ToString
+@Data // Getter + Setter + toString
+@NoArgsConstructor // 기본 생성자
+@AllArgsConstructor // 모든 필드 초기화용 매개 생성자
+public class Book {
+	// 필드
+	private String title;   // 제목
+	private String writer; // 작성자
+	private int price;	  // 가격
+	
+	// 메서드(생성자, getter/ setter/ toString())
+	
+}
---
+++ TodoList/src/main/java/edu/kh/todoList/model/dao/TodoListDAOImpl.java
@@ -0,0 +1,10 @@
+package edu.kh.todoList.model.dao;
+
+
+public class TodoListDAOImpl implements TodoListDAO{
+
+	// JDBC 객체 참조 변수 선언 + Properties 참조 변수 선언
+	
+	// TodoListDAOImpl 생성자 /xml/sqp.xml 경로 읽어오기
+	
+}
