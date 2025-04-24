205baa2b7b728db5d92c51d1aa108326353493da
Logan-01-for <sjson0105@gmail.com>
2025-04-24 17:46:53 +0900

    0 4 2 4

--- TodoList/src/main/java/edu/kh/todoList/common/JDBCTemplate.java
+++ TodoList/src/main/java/edu/kh/todoList/common/JDBCTemplate.java
@@ -7,16 +7,15 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Properties;
 
-
 public class JDBCTemplate {
-
+	
 	// 필드
 	private static Connection conn = null;
-	// -> static 메서드에 사용할 static 필드 
-	
+	// -> static 메서드에서 사용할 static 필드 선언
 	
 	
 	// 메서드
+	
 	/** 호출 시 Connection 객체를 생성해서 반환하는 메서드 + AutoCommit 끄기
 	 * @return conn
 	 */
@@ -27,59 +26,61 @@ public class JDBCTemplate {
 			// 이전에 참조하던 Connection 객체가 존재하고
 			// 아직 close() 된 상태가 아니라면
 			// 새로 만들지 않고 기존 Connection 반환
-			if (conn !=null && !conn.isClosed() ) {
+			if( conn != null && !conn.isClosed() ) {
 				return conn;
 			}
 			
-			// 1. Properties  객체 생성
+			// 1. Properties 객체 생성
 			Properties prop = new Properties();
 			
 			// 2. Properties가 제공하는 메서드를 이용해서 driver.xml 파일 내용을 읽어오기
 			// src/main/resources 경로상에 위치한 driver.xml 파일 읽어오기
-			String filePath = JDBCTemplate.class.getResource("/xml/diver.xml").getPath();
+			String filePath = JDBCTemplate.class.getResource("/xml/driver.xml").getPath();
 			// JDBCTemplate.class.getResource("경로") : 
-			// * 클래스 패스(classpath)내에서 지정된 리소스 파일을 찾는 메서드
+			// * 클래스패스(classpath) 내에서 지정된 리소스 파일을 찾는 메서드
 			
 			// * classpath란?
 			// Java 프로그램이 클래스를 찾기 위해 검색하는 경로를 의미
 			// -> src/main/resources 또는 WEB-INF/classes 에서 찾음
 			
-			// getPath() : URL 객체에서 실제 파일 시스템의 경로 중 절대경로 방법으로 얻어옴.
+			// getPath() : URL 객체에서 실제 파일 시스템의 경로를 절대경로 방법으로 얻어옴.
 			
 			System.out.println(filePath);
 			
 			prop.loadFromXML(new FileInputStream(filePath));
 			
 			
-			// 3. prop에 저장된 값을 이용해서 
-			// Connection 객체 생성
+			// 3. prop에 저장된값을 이용해서 Connection 객체 생성
 			Class.forName(prop.getProperty("driver"));
-			// Class.forName(oracle.jdbc.driver.T4CConnection@29176cc1);
+			// Class.forName("oracle.jdbc.driver.OracleDriver");
 			
-			conn = DriverManager.getConnection(prop.getProperty("url"),
-												prop.getProperty("userName"),	
-												prop.getProperty("password"));
-			// conn = DriverManager.getConnection(oracle.jdbc.driver.T4CConnection@29176cc1,
-			//										"todo_jdbc",	
-			//										"todoList1234");
+			conn = DriverManager.getConnection( prop.getProperty("url"),
+												prop.getProperty("userName"),
+												prop.getProperty("password") );
+			// conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE",
+			// 										"todo_jdbc",
+			//										"todoList1234")
 			
-			// 4. 만들어진 Connection에서 AutoCommit 끄기
+			// 4. 만들어진 Connection 에서 AutoCommit 끄기
 			conn.setAutoCommit(false);
+			
 			
 		} catch (Exception e) {
 			System.out.println("커넥션 생성 중 예외 발생..");
 			e.printStackTrace();
 		}
+		
 		return conn;
+		
 	}
 	
-	// -------------------------------------------------------------
+	
+	// ------------------------------------
 	
 	/** 전달 받은 커넥션에서 수행한 SQL을 Commit 하는 메서드
-	 * @param conn 
+	 * @param conn
 	 */
 	public static void commit(Connection conn) {
-		
 		
 		try {
 			if(conn != null && !conn.isClosed()) conn.commit();
@@ -89,7 +90,6 @@ public class JDBCTemplate {
 			e.printStackTrace();
 		}
 		
-		
 	}
 	
 	/** 전달 받은 커넥션에서 수행한 SQL을 ROLLBACK 하는 메서드
@@ -98,17 +98,18 @@ public class JDBCTemplate {
 	public static void rollback(Connection conn) {
 		
 		try {
-			if(conn !=null && !conn.isClosed()) conn.rollback();
+			if(conn != null && !conn.isClosed()) conn.rollback();
 			
 		} catch (Exception e) {
-		System.out.println("롤백 중 예외발생");
-		e.printStackTrace();
+			System.out.println("롤백 중 예외발생");
+			e.printStackTrace();
 		}
+		
 	}
 	
-	// -------------------------------------------------------------------
+	// ---------------------------------------------------------
 	
-	// 커넥션, Statement(PreparedStatement), ResultSet
+	// 커넥션, Statement(PreparedStatement), ResultSet 
 	
 	/** 전달받은 커넥션을 close(자원반환) 하는 메서드
 	 * @param conn
@@ -116,12 +117,13 @@ public class JDBCTemplate {
 	public static void close(Connection conn) {
 		
 		try {
-			if(conn !=null && !conn.isClosed()) conn.close();
+			if(conn != null && !conn.isClosed()) conn.close();
 			
 		} catch (Exception e) {
-		System.out.println("커넥션 close() 중 예외발생");
-		e.printStackTrace();
+			System.out.println("커넥션 close() 중 예외발생");
+			e.printStackTrace();
 		}
+		
 	}
 	
 	/** 전달 받은 Statement or PreparedStatement 둘 다 close()하는 메서드
@@ -132,12 +134,13 @@ public class JDBCTemplate {
 	public static void close(Statement stmt) {
 		
 		try {
-			if(stmt !=null && !stmt.isClosed()) stmt.close();
+			if(stmt != null && !stmt.isClosed()) stmt.close();
 			
 		} catch (Exception e) {
 			System.out.println("Statement close()중 예외 발생");
 			e.printStackTrace();
 		}
+		
 	}
 	
 	/** 전달 받은 ResultSet을 close() 하는 메서드
@@ -146,11 +149,11 @@ public class JDBCTemplate {
 	public static void close(ResultSet rs) {
 		
 		try {
-			if(rs !=null && !rs.isClosed())rs.close();
+			if(rs != null && !rs.isClosed()) rs.close();
 			
 		} catch (Exception e) {
-		System.out.println("ResultSet close()중 예외 발생");
-		e.printStackTrace();
+			System.out.println("ResultSet close() 중 예외 발생");
+			e.printStackTrace();
 		}
 		
 	}
@@ -160,4 +163,5 @@ public class JDBCTemplate {
 	
 	
 	
+
 }
